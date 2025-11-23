fe89ae879ae547a41ffaa896c8a63d2598086a2f
Benjamín Vicente <projects@benjavicente.dev>
2025-11-23 02:48:25 -0300

    nitro

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
@@ -14,6 +15,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 });
