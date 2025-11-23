414ab6e0d6f5ff5703279ba620a90148031bbc88
Tomas Fernandez <tfernandez@buk.cl>
2025-11-22 21:45:52 -0300

    delete sso sources

--- apps/web/src/components/auth/LoginForm.tsx
+++ apps/web/src/components/auth/LoginForm.tsx
@@ -115,68 +115,6 @@ export function LoginForm() {
               />
 
               <SocialLoginButton
-                provider="github"
-                label="Continue with GitHub"
-                disabled={true}
-                icon={
-                  <svg
-                    className="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                }
-              />
-
-              <SocialLoginButton
-                provider="microsoft"
-                label="Continue with Microsoft"
-                disabled={true}
-                icon={
-                  <svg className="w-5 h-5" viewBox="0 0 23 23">
-                    <path fill="#f35325" d="M1 1h10v10H1z" />
-                    <path fill="#81bc06" d="M12 1h10v10H12z" />
-                    <path fill="#05a6f0" d="M1 12h10v10H1z" />
-                    <path fill="#ffba08" d="M12 12h10v10H12z" />
-                  </svg>
-                }
-              />
-
-              <SocialLoginButton
-                provider="facebook"
-                label="Continue with Facebook"
-                disabled={true}
-                icon={
-                  <svg className="w-5 h-5" viewBox="0 0 24 24">
-                    <path
-                      fill="#1877F2"
-                      d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
-                    />
-                  </svg>
-                }
-              />
-
-              <SocialLoginButton
-                provider="apple"
-                label="Continue with Apple"
-                disabled={true}
-                icon={
-                  <svg
-                    className="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.74 1.18 0 2.45-1.64 3.57-1.64.6 0 1.48.16 2.05.83-3.06 1.21-2.53 5.72.4 6.94-.31 1.33-.96 2.35-1.1 2.75-.52 1.5-1.4 2.55-2.05 2.75zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z" />
-                  </svg>
-                }
-              />
-
-              <SocialLoginButton
                 provider="passkey"
                 label="Sign in with Passkey"
                 onClick={handlePasskeyLogin}
